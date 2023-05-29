0522da62689c7e471c766eb265088c535f052ec2
Goulandis <1612160117@qq.com>
2023-05-29 12:13:30 +0800

    2023.05.29 12:13

--- GetNames.py
+++ GetNames.py
@@ -1,17 +1,35 @@
 import os
+from tqdm import tqdm
 
-folder_path = 'D:\Goulandis\SelfMusics\Tmp'  # Replace 'folder_path' with the actual path to the folder containing the .lrc files
+folder_path = '..\SelfMusics'  # 替换为实际的文件夹路径
 
-# Get a list of all files in the folder
-file_list = os.listdir(folder_path)
+# 清空name.txt文件内容
+with open('README.md', 'w') as output_file:
+    pass
 
-# Filter the files to only include those with the .lrc extension
-lrc_files = [file for file in file_list if file.endswith('.lrc')]
+# 获取目录下所有文件夹的名字
+folder_names = [name for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
+folder_names.remove('.git')
+folder_names.remove('AIMP')
+folder_names.remove('Tmp')
 
-# Create and open the name.txt file for writing
-with open('name.txt', 'w') as output_file:
-    # Write each filename to a new line in the file
-    for lrc_file in lrc_files:
-        output_file.write(lrc_file + '\n')
+mp3_files = []
 
-print("Filenames have been stored in name.txt.")
+output_file = open('README.md', 'a',encoding='utf-8')
+
+# 遍历每个文件夹，获取子目录中后缀为.mp3的文件名
+for folder_name in tqdm(folder_names, desc='读取文件夹'):
+    output_file.write('\n' + '---' + '\n' + folder_name + '\n --- \n' + '---' + '\n')
+    subfolder_path = os.path.join(folder_path, folder_name)
+    for root, dirs, files in os.walk(subfolder_path):
+        for file in tqdm(files,desc='开始写入目录'+folder_name+'中的文件'):
+            if file.endswith('.mp3'):
+                mp3_files.append(file)
+                filename = file.strip('.mp3')
+                output_file.write('- ' + filename + '\n')
+
+print("文件名已写入name.txt。")
+
+output_file.close();
+
+
--- RunGetNames.bat
+++ RunGetNames.bat
@@ -1,1 +1,8 @@
 python GetNames.py
+set d=%date:~0,4%
+git add .
+echo %date:~0,4%.%date:~5,2%.%date:~8,2% %time:~0,2%:%time:~3,2%
+pause
+git commit -m "%date:~0,4%.%date:~5,2%.%date:~8,2% %time:~0,2%:%time:~3,2%"
+git push
+pause
